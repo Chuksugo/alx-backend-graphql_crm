d77c3072b5d7f61eb2338a8c04e9f8fa5a1d46c6
ugochukwuemeka <chukwuemekaugochukwu00@gmail.com>
2025-08-27 15:56:07 +0100

    Fix schema.py with Query inheriting from CRMQuery and graphene.ObjectType

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,6 +1,7 @@
 import graphene
+from crm.schema import CRMQuery   # <-- import CRMQuery from crm app schema
 
-class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hello, GraphQL!")
+class Query(CRMQuery, graphene.ObjectType):
+    pass   # No need to add fields here unless required
 
 schema = graphene.Schema(query=Query)
